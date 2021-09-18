aba9d945cae051a93ac28dda01485390028352e4
AÏe !! <makramjandar@gmail.com>
2021-09-19 00:58:01 +0200

    Update Expand-URL.ps1

--- Expand-URL.ps1
+++ Expand-URL.ps1
@@ -1,14 +1,44 @@
-function Expand-Uri($Uri)
-{
-    begin {
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+<#
+.SYNOPSIS
+Retrieves shortened URI.
+.PARAMETER SHORT_URIS
+The shortened URIs.
+.PARAMETER LOG
+The path and filename of a text file where failed SHORT_URIS will be logged. Defaults to $env:TEMP\ExpandURI.log.
+.EXAMPLE
+(Expand-URI $URI).LONG_URI
+.EXAMPLE
+URI1, URI2 | Expand-Uri
+.EXAMPLE
+. { iwr -useb git.io/JzqqY } | iex; Expand-URI URL1 URL2
+#>
+
+Function Expand-URI {
+    [CmdletBinding()]
+    Param(
+
+        [Parameter(Mandatory = $True, ValueFromPipeline = $True, ValueFromPipelinebyPropertyName = $True)]
+        [string[]]$SHORT_URIS,
+        [string]$LOG = "$env:TEMP\ExpandURI.log"
+    )
+
+    BEGIN {
+        Remove-Item $LOG –erroraction silentlyCONTINUE
     }
-    process {
-        try {return (Invoke-WebRequest -Uri $Uri -UseBasicParsing).BaseResponse.ResponseUri.AbsoluteUri}}
-        catch {"Ran into an issue: $PSItem"}
+    PROCESS {
+        Foreach ($SHORT_URI in $SHORT_URIS) {
+            $CONTINUE = $true
+            try {
+                $LONG_URI = (iwr -Uri $SHORT_URI -UseBasicParsing).BaseResponse.ResponseUri.AbsoluteUri
+            } 
+            catch { $CONTINUE = $false ; $PSItem.InvocationInfo | Format-List * | Out-File $LOG }
+            if ($CONTINUE) {
+                $OBJ = New-Object –typename PSObject
+                $OBJ `
+                | Add-Member –membertype NoteProperty –name SHORT_URI –value $SHORT_URI –passthru `
+                | Add-Member –membertype NoteProperty –name LONG_URI –value $LONG_URI
+                Write-Output $OBJ
+            }
+        }
     }
 }
-
-# Expand-Uri https://t.co/rHrCyVMNA3
-# buff.ly/2sWvPOH | Expand-Uri
-# iex ((New-Object System.Net.WebClient).DownloadString('git.io/JzqqY'))
