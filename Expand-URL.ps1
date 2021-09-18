51f92e8b3cb7572a636f83e2c41a4543c41d929a
AÏe !! <makramjandar@gmail.com>
2021-09-19 01:53:27 +0200

    Update Expand-URL.ps1

--- Expand-URL.ps1
+++ Expand-URL.ps1
@@ -1,49 +1,52 @@
-<#
-.SYNOPSIS
-Retrieves shortened URI.
-.PARAMETER SHORT_URIS
-The shortened URIs.
-.PARAMETER LOG
-The path and filename of a text file where failed SHORT_URIS will be logged. Defaults to $env:TEMP\ExpandURI.log.
-.EXAMPLE
-(Expand-URI $URI).LONG_URI
-.EXAMPLE
-URI1, URI2 | Expand-Uri
-.EXAMPLE
-. { iwr -useb git.io/JzqqY } | iex; Expand-URI URL1 URL2
-#>
+  new-module -name Expand-URI -scriptblock {  
+    <#
+    .SYNOPSIS
+    Retrieves shortened URI.
+    .PARAMETER SHORT_URIS
+    The shortened URIs.
+    .PARAMETER LOG
+    The path and filename of a text file where failed SHORT_URIS will be logged. Defaults to $env:TEMP\ExpandURI.log.
+    .EXAMPLE
+    (Expand-URI $URI).LONG_URI
+    .EXAMPLE
+    URI1, URI2 | Expand-Uri
+    .EXAMPLE
+    . { iwr -useb git.io/JzqqY } | iex; Expand-URI URL1 URL2
+    #>
 
-Function Expand-URI {
-    [CmdletBinding()]
-    Param(
+    Function Expand-URI {
+        [CmdletBinding()]
+        Param(
 
-        [Parameter(Mandatory = $True, ValueFromPipeline = $True, ValueFromPipelinebyPropertyName = $True)]
-        [string[]]$SHORT_URIS,
-        [string]$LOG = "$env:TEMP\ExpandURI.log"
-    )
+            [Parameter(Mandatory = $True, ValueFromPipeline = $True, ValueFromPipelinebyPropertyName = $True)]
+            [string[]]$SHORT_URIS,
+            [string]$LOG = "$env:TEMP\ExpandURI.log"
+        )
 
-    BEGIN {
-        Remove-Item $LOG –erroraction silentlyCONTINUE
-    }
-    PROCESS {
-        Foreach ($SHORT_URI in $SHORT_URIS) {
-            $CONTINUE = $true
-            try {
-                $LONG_URI = (iwr -Uri $SHORT_URI -UseBasicParsing).BaseResponse.ResponseUri.AbsoluteUri
-            } 
-            catch { $CONTINUE = $false ; $PSItem.InvocationInfo | Format-List * | Out-File $LOG }
-            if ($CONTINUE) {
-                $OBJ = New-Object –typename PSObject
-                $OBJ `
-                | Add-Member –membertype NoteProperty –name SHORT_URI –value $SHORT_URI –passthru `
-                | Add-Member –membertype NoteProperty –name LONG_URI –value $LONG_URI
-                Write-Output $OBJ
+        BEGIN {
+            Remove-Item $LOG –erroraction silentlyCONTINUE
+        }
+        PROCESS {
+            Foreach ($SHORT_URI in $SHORT_URIS) {
+                $CONTINUE = $true
+                try {
+                    $LONG_URI = (iwr -Uri $SHORT_URI -UseBasicParsing).BaseResponse.ResponseUri.AbsoluteUri
+                } 
+                catch { $CONTINUE = $false ; $PSItem.InvocationInfo | Format-List * | Out-File $LOG }
+                if ($CONTINUE) {
+                    $OBJ = New-Object –typename PSObject
+                    $OBJ `
+                    | Add-Member –membertype NoteProperty –name SHORT_URI –value $SHORT_URI –passthru `
+                    | Add-Member –membertype NoteProperty –name LONG_URI –value $LONG_URI
+                    Write-Output $OBJ
+                }
             }
         }
     }
+
+set-alias Expand-URI -value Expand-URI
+export-modulemember -function 'Expand-URI' -alias 'Expand-URI'
 }
-
-
 
 #Expand-Uri 'buff.ly/2sWvPOH'
 #. { iwr -useb git.io/JzqqY } | iex; Expand-URI -SHORT_URI 'buff.ly/2sWvPOH'
