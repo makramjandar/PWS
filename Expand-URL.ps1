bb907c6532cfe11e9788ae4b473dc4f14a043a6b
AÏe !! <makramjandar@gmail.com>
2021-09-19 13:06:15 +0200

    Update Expand-URL.ps1

--- Expand-URL.ps1
+++ Expand-URL.ps1
@@ -30,7 +30,7 @@ new-module -name Expand-URI -scriptblock {
                 $CONTINUE = $true
                 try {
                     $LONG_URI = (iwr -Uri $SHORT_URI -UseBasicParsing).BaseResponse.ResponseUri.AbsoluteUri
-                    Write-Error -Message "Houston, we have a problem." -ErrorAction Stop
+                    Write-Error $LONG_URI -Message "Houston, we have a problem." -ErrorAction Stop
                 } 
                 catch {$CONTINUE = $false ; $PSItem.InvocationInfo | Out-File $LOG; "See $LOG"}
                 if ($CONTINUE) {
